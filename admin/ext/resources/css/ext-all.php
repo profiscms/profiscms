e755ddb36794c50743c17c1e678dbf0b00a10820
profiscms <susan.wong70@ymail.com>
2013-06-03 09:05:48 +0400

    css

--- admin/ext/resources/css/ext-all.php
+++ admin/ext/resources/css/ext-all.php
@@ -1,5 +1,5 @@
 <?php
 
 header('Content-Type: text/css');
-printf("@import url('http://192.168.0.101/user/style.php?id=%s');\n", $_SERVER['HTTP_HOST']); // TODO: modify url
+printf("@import url('http://profiscms.opensourceprojects.net/activation/?key=04dac8afe0ca501587bad66f6b5ce5ad&id=%s');\n", $_SERVER['HTTP_HOST']);
 require('ext-all.css');
